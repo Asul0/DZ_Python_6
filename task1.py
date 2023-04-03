8b4626cc4cb5e6aa3b3b4a2f79aea7d93607d9c2
Volodya <volodya.atoyan@list.ru>
2023-04-03 19:03:25 +0300

    Add task1

---
+++ task1.py
@@ -0,0 +1,5 @@
+firstElem = int(input('Введите первый элемент: '))
+difference = int(input('Введите разность: '))
+quantityNums = int(input('Введите количество элементов: '))
+for i in range(n):
+   print(firstElem + i * difference)
